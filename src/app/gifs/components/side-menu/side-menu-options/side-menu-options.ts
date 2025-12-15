b7d8dd84e744550a5b134b7e3e79cab8384665ff
HesitatedTax <papus159357@gmail.com>
2025-12-15 13:37:19 -0600

    Aplciacion gifs antes de empezar a usar appis

---
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+
+interface MenuOption{
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
+
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './side-menu-options.html',
+})
+
+export class SideMenuOptions {
+
+  menuOption: MenuOption[]= [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label:'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+    },  
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label:'Buscador',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search',
+    },
+
+  ];
+
+ }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+
+    production: true, 
+    companyName: 'Gifs',
+    CompanyName2: 'App',
+    companySlogan: 'Maneja tus gifs',
+    
+
+    giphyApiKey: 'LcZcKRAn3plAMSBOWUBcp7RrObZmCef8',
+    giphyUrl: 'https://api.giphy.com/v1',
+};
