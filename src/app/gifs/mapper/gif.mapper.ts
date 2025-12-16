54799d1ba51d9e8c6f3e14224642e09e12a2af26
HesitatedTax <papus159357@gmail.com>
2025-12-16 12:07:57 -0600

    implementacion del buscador de gifs

---
+++ src/app/gifs/mapper/gif.mapper.ts
@@ -0,0 +1,15 @@
+import { Gif } from "../interfaces/gif.interface";
+import { GiphyItem } from "../interfaces/giphy.interfaces";
+
+export class GifMapper {
+    static mapGipghyItemToGif( item: GiphyItem ): Gif {
+return{
+    id: item.id,
+    title: item.title,
+    url: item.images.original.url,
+}
+    }
+    static mapGiphyItemsToGifArray(items: GiphyItem[]): Gif[] {
+    return items.map(GifMapper.mapGipghyItemToGif);
+}     
+}
